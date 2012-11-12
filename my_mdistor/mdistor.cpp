22962e5856a3a0bb95832f9936daca66d03e4ea4
Tao Ke <tao@TKDesktop.localdomain>
2012-11-11 22:21:43 -0500

    Clearned up

--- my_mdistor/mdistor.cpp
+++ my_mdistor/mdistor.cpp
@@ -22,23 +22,23 @@ void MDistor::insert(string  ns, BSONObj & p) {
     BSONObjBuilder key;
     key.append("type", MDIS::KEYTYPE);
     key.append("ns", ns);
-    cout<<"Before querying chunkcoll"<<endl;
+    //cout<<"Before querying chunkcoll"<<endl;
     auto_ptr<DBClientCursor> keyCursor = dbConn->query(chunkColl, key.obj());
-    cout<<"Done querying chunkColl"<<endl;
+    //cout<<"Done querying chunkColl"<<endl;
     
     //string keyS = keyCursor->next().getStringField("key");;
 
-    cout<<"Getting worker connection"<<endl;
+    //cout<<"Getting worker connection"<<endl;
     //shared_ptr<DBClientConnection> worker = getWorkerConnection(keyS,  p); 
     shared_ptr<DBClientConnection> worker = getWorkerConnection(defaultKeys[ns], p);
 
-    cout << "Attempting to insert "<< ns << endl;
+    //cout << "Attempting to insert "<< ns << endl;
     worker->insert(ns, p);
 }
 
 void MDistor::setKey(string ns, BSONObj & keyAndRange ) {
     //TODO: is it necessary to redeploy existing data if the key changes?
-    cout<<"Starting to set keys"<<endl;
+    //cout<<"Starting to set keys"<<endl;
     string db = ns;
     string chunkColl = MDIS::CHUNKS + db; 
     cout<<" is : "<<chunkColl<<endl;
@@ -61,14 +61,14 @@ void MDistor::setKey(string ns, BSONObj & keyAndRange ) {
     key.append("ns", ns);
     key.append("key", keyAndRange.getStringField("key"));
 
-    cout<<"trying to insert to chunk collection "<<endl;
+    //cout<<"trying to insert to chunk collection "<<endl;
     dbConn->insert(chunkColl, chunk.obj());
-    cout<<"Done one"<<endl;
+    //cout<<"Done one"<<endl;
     dbConn->insert(chunkColl, key.obj());
-    cout<<"Done all "<<endl;
+    //cout<<"Done all "<<endl;
 
     string coll = ns.substr(0,ns.rfind('.'));
-    cout<<"--------"<<coll<<"----------------"<<endl;
+    //cout<<"--------"<<coll<<"----------------"<<endl;
     defaultKeys[coll] = ns;
 }
 
@@ -88,7 +88,7 @@ void MDistor::addWorker(string host) {
         worker->connect(host);
         workers[worker_string] = worker;
     }
-    cout<<"Done adding workers"<<endl;
+    //cout<<"Done adding workers"<<endl;
 
 }
 
@@ -98,12 +98,12 @@ void MDistor::getWorkers() {
     while(cursor->more()) {
         //string host = cursor->next()["host"];
         string host = cursor->next().getStringField("host");
-        cout << "getWorkers: host is: " << host <<endl;
+        //cout << "getWorkers: host is: " << host <<endl;
         shared_ptr<DBClientConnection> conn(new DBClientConnection());
         conn->connect(host);
         workers[host] = conn; 
     }
-    cout<<"Done getting workers"<<endl;
+    //cout<<"Done getting workers"<<endl;
 }
 
 void MDistor::init() {
@@ -117,25 +117,28 @@ void MDistor::init() {
  *  cursor: cursor for the key query.
  */
 shared_ptr<DBClientConnection> MDistor::getWorkerConnection(string key, BSONObj & p )  {
-    cout<<"getworker, key is:"<<key<<endl;
+    //cout<<"getworker, key is:"<<key<<endl;
     string _key = key.substr(key.rfind('.')+1);
-    cout<<"_key is :"<<_key<<"|"<<endl;
-    cout<<"p is :"<<p.toString()<<endl;
+    //cout<<"_key is :"<<_key<<"|"<<endl;
+    //cout<<"p is :"<<p.toString()<<endl;
     mongo::BSONElement  field = p.getField("id");
-    cout<<"getWorker, field is :"<<field.Int()<<endl;
+    //cout<<"getWorker, field is :"<<field.Int()<<endl;
     //auto_ptr<DBClientCursor>  cursor = dbConn->query(MDIS::CHUNKS, BSON("key.min" << BSON("$lte" << p.getStringField(key.c_str())) << "key.max" << BSON("$gte" << p.getStringField(key.c_str()))));
-    auto_ptr<DBClientCursor> cursor = dbConn->query(MDIS::CHUNKS, QUERY("key.max"<<GT<<field.Int()));
-
     string mdistor_ns = MDIS::CHUNKS + key;
+    auto_ptr<DBClientCursor> cursor = dbConn->query(mdistor_ns, QUERY("key.max"<<GTE<<field.Int()));
+/*
     cout << "mdistor_ns:"<<mdistor_ns <<endl;
     //auto_ptr<DBClientCursor> testCur = dbConn->query(mdistor_ns, QUERY("key.max"<<"100"));
     auto_ptr<DBClientCursor> testCur = dbConn->query(mdistor_ns, QUERY("key.min" <<LT<< 10));
     cout<<"testCur is : "<<testCur->next().getStringField("host");
-
+*/
     if(cursor->more()) {
         cout<<"trying to get host"<<endl;
         string host = cursor->next().getStringField("host");
+        cout<<"host is "<<host<<endl;
         return workers[host];
+    } else {
+        cout<<"cursor doesn't have more"<<endl;
     }
 
     //TODO: need to add new chunks if there is no corresponding chunk
