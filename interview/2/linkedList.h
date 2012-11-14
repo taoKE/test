abf4f4397246c033eecb79396dfcdf555b1151ad
Tao_secondary <tao_secondary@tke-mac-7.local>
2012-11-14 13:14:29 -0500

    Added 2_4

--- interview/2/linkedList.h
+++ interview/2/linkedList.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <iostream>
+
+using namespace std;
+
 template<class T>
 class LinkedListNode {
     private: 
@@ -29,16 +33,35 @@ template<class T>
 class LinkedList{
     private:
         LinkedListNode<T> * head;
+        int size;
 
     public:
-        LinkedList(LinkedListNode<T> * _h):head(_h){}
+        LinkedList(LinkedListNode<T> * _h):head(_h){
+            if(head != NULL) size = 1;
+            else size = 0;
+        }
+
+        int getSize() {
+            return size;
+        }
 
         void append(LinkedListNode<T> * _next) {
             LinkedListNode<T> * current = head;
-            while(current->getNext() != NULL){
-                current = current->getNext();
+            if(current == NULL) {
+                current = _next;
+                head = _next;
+            } else {
+                while(current->getNext() != NULL){
+                    current = current->getNext();
+                }
+                current->setNext( _next);
+                size++;
             }
-            current->setNext( _next);
+        }
+
+        void headAppend(LinkedListNode<T> * newHead){
+            newHead->setNext(head);
+            head = newHead;
         }
 
         LinkedListNode<T> * getHead() {
@@ -47,6 +70,14 @@ class LinkedList{
 
         void setHead(LinkedListNode<T> * h) {
             head = h;
+        }
+
+        void printList() {
+            LinkedListNode<T> * temp = head;
+            while(temp != NULL) {
+                cout<<temp->getValue()<< " ";
+                temp = temp->getNext();
+            }
         }
 
         ~LinkedList(){
